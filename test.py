fa11b6bf2230f8e0c1b883d675a48f88ae060227
Grégory Allard <djolescro94@gmail.com>
2019-06-24 23:17:35 +0200

    ajout d'un fichier 'test' en python

---
+++ test.py
@@ -0,0 +1,7 @@
+age = 24
+if age < 18 and age > 0:
+    print("Tu as ",age, " ans, donc tu ne peux pas rentrer")
+elif age >= 18:
+    print("Tu as ",age, " ans, donc tu peux rentrer")
+else:
+    print("tu as saisis un nombre incorrect ! FDP")
