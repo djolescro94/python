8c583c336181bbf4e0d7e59ab105c6705f49ce98
Grégory Allard <djolescro94@gmail.com>
2019-06-25 00:23:13 +0200

    Création d'un fichier .py qui demande à l'utilisateur un nombre, et une boucle va accrémenter la variable nombre jusqu'à ce qu'elle soit égale a 100 exactement

--- boucle.py
+++ boucle.py
@@ -1,1 +1,13 @@
-while nombre <= 100:
+nombre = int(input("Saisissez votre nombre de tour actuel : "))
+
+while nombre < 0 or nombre > 100:
+    print("Saisie incorrecte")
+    nombre = int(input("Saisissez votre nombre de tour actuel : "))
+    
+while nombre <= 100 and nombre >= 0:
+    if nombre == 1:
+        print("j'ai fait ", nombre ,"tour !")
+        nombre = nombre + 1
+    else:
+        print("j'ai fait ", nombre ,"tours !")
+        nombre = nombre + 1
