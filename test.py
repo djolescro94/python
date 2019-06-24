20300116ba86be5a47a441e2811e4ca368b80e4f
Grégory Allard <djolescro94@gmail.com>
2019-06-24 23:53:12 +0200

    conversion du str en int sur l'input age, qui causait une erreur

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
-age = 24
+age = int(input("Quel âge as-tu ? "))
 if age < 18 and age > 0:
     print("Tu as ",age, " ans, donc tu ne peux pas rentrer")
 elif age >= 18:
     print("Tu as ",age, " ans, donc tu peux rentrer")
 else:
-    print("tu as saisis un nombre incorrect ! FDP")
+    print("tu as saisis un nombre incorrect ! FDP", age)
